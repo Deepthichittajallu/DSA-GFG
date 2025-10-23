3d801a150d1dfa11ada12fe58704cdbd38820d9c
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2025-10-23 16:09:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // code here
+        sort(points.begin(),points.end(),[](vector<int>a,vector<int>b)
+        {
+            return (double)sqrt((a[0]*a[0]) + a[1]*a[1]) < (double)sqrt((b[0]*b[0]) + b[1]*b[1]);
+        });
+        return {points.begin(),points.begin()+k};
+    }
+};
