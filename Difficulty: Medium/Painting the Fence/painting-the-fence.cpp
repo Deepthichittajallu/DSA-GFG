49e5fbee29342f9e07e21065e74ffa2f526547e1
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-04-02 22:11:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countWays(int n, int k) {
+        // code here
+        vector<int>dp(n+1,0);
+        if(n==1) return k;
+        if(n==2) return k*k;
+        dp[1] = k;
+        dp[2] = k*k;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i] = dp[i-1] * (k-1) + dp[i-2] * (k-1);
+        }
+        return dp[n];
+    }
+};
